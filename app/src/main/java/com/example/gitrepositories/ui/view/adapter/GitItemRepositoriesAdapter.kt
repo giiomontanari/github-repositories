618dfdaf419d67c5e50b840f2e0526e8ed2097c1
giokaze <giovannimontanari3@gmail.com>
2022-01-16 00:25:59 -0300

    completing the repositories item layout

--- app/src/main/java/com/example/gitrepositories/ui/view/adapter/GitItemRepositoriesAdapter.kt
+++ app/src/main/java/com/example/gitrepositories/ui/view/adapter/GitItemRepositoriesAdapter.kt
@@ -4,6 +4,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.gitrepositories.R
 import com.example.gitrepositories.data.dto.GitItemPullRequestDTO
 import kotlinx.android.synthetic.main.item_gititemrepositories.view.*
@@ -29,7 +31,16 @@ class GitItemRepositoriesAdapter(
 
         fun bind(item: GitItemPullRequestDTO) {
             with(itemView) {
-                item_title.text = item.titlePullRequest
+                name_autor.text = item.user.login
+                title_pr.text = item.titlePullRequest
+                body_pr.text = item.body
+            }
+            with(itemView.avatar_autor) {
+                Glide.with(this)
+                    .load(item.user.avatarUrl)
+                    .apply(RequestOptions.circleCropTransform())
+                    .placeholder(R.drawable.ic_git)
+                    .into(this)
             }
         }
     }
